c510ed4de91d3751b071a70885fef2351c7bdb9b
Enrique <octepc.97@gmail.com>
2022-07-20 10:06:59 -0500

    reviewd lists in python

---
+++ basics.py
@@ -0,0 +1,9 @@
+def run():
+    print('This function runs the program')
+    frase = 'This is a phrase as a string'
+    frase = frase.upper().lower()
+    print(frase)
+
+
+if __name__ == '__main__':
+    run()
---
+++ lists.py
@@ -0,0 +1,26 @@
+list_elements = ["elemento_1", "elemento_2", "elemento_3"]
+
+
+# access to a specific element in a list
+print(list_elements[0])
+
+# getting the length of an array using len()
+print(len(list_elements))
+
+
+boolean_list = [True, False, True]
+
+print(boolean_list)
+
+integers_list = [1, 2, 3, 4, 5, 6]
+print(integers_list)
+
+float_list = [1.2, 2.3, 3.4, 4.5, 5.6]
+
+print(float_list)
+
+
+print(type(float_list))
+
+new_list = list((10, 20, 30, 40, 50))
+print(new_list)
